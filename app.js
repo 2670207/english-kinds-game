21e15fa12dad8e7d21b1683558b7f26b28ce2561
besttex server <info@besttex.by>
2021-07-06 00:33:52 +0300

    fix

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ async function start() {
       useUnifiedTopology: true,
       useCreateIndex: true
     })
-    app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+    app.listen('', () => console.log(`App has been started on port ${PORT}...`))
   } catch (e) {
     console.log('Server Error', e.message)
     process.exit(1)
